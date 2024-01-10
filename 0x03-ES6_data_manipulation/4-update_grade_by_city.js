1701b3ffef13f6c8db787ecf8b177cf9c7767330
Nwalozie <iyecknwalozie@gmail.com>
2024-01-10 03:40:01 +0100

    ES6 data manipulation

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,14 @@
 export default function updateStudentGradeByCity(studentList, city, newGrades) {
-    return studentList
-      .filter(student => student.location === city)
-      .map(student => {
-        const matchingGrade = newGrades.find(grade => grade.studentId === student.id);
-  
-        return {
-          id: student.id,
-          firstName: student.firstName,
-          location: student.location,
-          grade: matchingGrade ? matchingGrade.grade : 'N/A',
-        };
-      });
-  }
+  return studentList
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
+
+      return {
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: matchingGrade ? matchingGrade.grade : 'N/A',
+      };
+    });
+}
