5e9665f01af63bed4e0a685e5a80c3fbd4609c24
Nwalozie <iyecknwalozie@gmail.com>
2024-01-13 00:32:59 +0100

    Data Manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+export default function cleanSet(set, startString) {
+  let res;
+  if (!startString || typeof startString !== 'string') {
+    return '';
+  }
+  res = Array.from(set).filter((word) => typeof word === 'string' && word.startsWith(startString));
+  res = res.map((word) => word.slice(startString.length));
+  return res.join('-');
+}
